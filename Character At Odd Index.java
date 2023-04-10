61857eeb14a455bd67872c1326fff46618364b17
shruthisinha <83690602+shruthisinha@users.noreply.github.com>
2023-04-10 10:55:07 +0530

    Sunny: u n

--- Character At Odd Index.java
+++ Character At Odd Index.java
@@ -7,7 +7,7 @@ public class CharAtOddIndex {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		String str=br.readLine();
 	
-		for(int i=0;i<str.length();i++) {  //index initialization at 0
+		for(int i=0;i<str.length();i++) {  
 
 			if(i%2!=0) {
 				System.out.println(str.charAt(i));
