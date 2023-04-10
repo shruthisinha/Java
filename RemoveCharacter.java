59e1a54a4a3cb1388da189e735155fb9d0e884c7
shruthisinha <83690602+shruthisinha@users.noreply.github.com>
2023-04-10 10:53:48 +0530

    Create RemoveCharacter.java

---
+++ RemoveCharacter.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class RemoveChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String ch = sc.nextLine();
+        String s = str.replace(ch,"");
+        System.out.println(s);
+    }
+}
